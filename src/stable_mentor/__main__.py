8610f6499c3e9a36f17174223fbf85f1c35560c1
Calvin Giles <calvin@fiit.tv>
2020-06-09 14:06:46 +1200

    Take data path as arg

--- src/stable_mentor/__main__.py
+++ src/stable_mentor/__main__.py
@@ -1,9 +1,12 @@
+import optparse
+import argparse
+
 from stable_mentor import stable_matching
 from stable_mentor.data_loader import load_data, parse_data
 
 
-def main():
-    df = load_data("../data/testdata9.xlsx")
+def main(data_path):
+    df = load_data(data_path)
     preferred_rankings_mentor, preferred_rankings_mentee = parse_data(df)
 
     tentative_engagements = stable_matching(preferred_rankings_mentor, preferred_rankings_mentee)
@@ -11,4 +14,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_path")
+    args = parser.parse_args()
+    main(args.data_path)
